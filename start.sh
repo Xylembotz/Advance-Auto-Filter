3f0520beb6012e6be9b6a608ef01d2902f62c236
Xylembotz <161294187+Xylembotz@users.noreply.github.com>
2024-02-26 16:14:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-Auto-Filter /Advance-Auto-Filter
+  git clone https://github.com/XylemBotz/Xylem-Auto-Filter /Xylem-Auto-Filter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-Auto-Filter
+  git clone $UPSTREAM_REPO /Xylem-Auto-Filter
 fi
-cd /Advance-Auto-Filter
+cd /Xylem-FILTER-BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
